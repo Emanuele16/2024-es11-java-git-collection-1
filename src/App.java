f7964019f96f2b00e8f0eecfb8cd58f0a063515f
Emanuele16 <81881987+Emanuele16@users.noreply.github.com>
2024-06-26 00:29:03 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,30 +1,126 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class App {
-
-    /**
-     * TODO:
-     * 
-     * completare la classe DataHelper
-     * 
-     * aggiungere i seguenti metodi per testare la classe DataHelper:
-     * - testRandomInteger : void
-     * testare le 3 implementazioni del metodo getRandomInteger
-     * 
-     * - testRandomUniqueInteger : void
-     * testare le 3 implementazioni del metodo getRandomUniqueInteger
-     * 
-     * - testFrequencyMap : void
-     * testare il metodo getFrequencyMap;
-     * sfruttare i metodi precedentemente svolti e utilizzarli come
-     * dati in ingresso per il metodo getFrequencyMap
-     * 
-     */
-
+    static Scanner scan;
     public static void main(String[] args) throws Exception {
-        System.out.println(DataHelper.getRandomInteger(2));
-        System.out.println(DataHelper.getRandomInteger(2, 2));
-        System.out.println(DataHelper.getRandomInteger(5, 3, 6));
+        scan = new Scanner(System.in);
+        TestRandomInteger();
+        TestRandomUniqueInteger();
+        testFrequencyMap();
+    }
+
+    public static void TestRandomInteger(){
+        getRandomInteger();
+        getRandomIntegerUntilMax();
+        getRandomIntegerBetweenMinMax();
+    }
+
+    public static void TestRandomUniqueInteger(){
+        getRandomUniqueInteger();
+        getRandomUniqueIntegerUntilMax();
+        getRandomUniqueIntegerBetweenMinMax();
+    }
+
+     public static void getRandomInteger() {
+        int size = 0;
+
+        while(size<=0){
+            System.out.print("inserisci la grandezza della lista (tutti i numeri eccetto 0): ");
+            size = scan.nextInt();
+        }
+
+        System.out.println(DataHelper.getRandomInteger(size));
+    }
+
+    public static void getRandomIntegerUntilMax() {
+        int size = 0, max = 0;
+
+        while(size<=0){
+            System.out.print("inserisci la grandezza della lista (tutti i numeri eccetto 0): ");
+            size = scan.nextInt();
+        }
+
+        System.out.print("inserisci il valore max della numerazione: ");
+        max = scan.nextInt();
+
+        System.out.println(DataHelper.getRandomInteger(size, max));
+    }
+
+    public static void getRandomIntegerBetweenMinMax() {
+        int size = 0, min = 0, max = 0;
+
+        while(size<=0){
+            System.out.print("inserisci la grandezza della lista (tutti i numeri eccetto 0): ");
+            size = scan.nextInt();
+        }
+        
+        System.out.print("inserisci il valore min della numerazione: ");
+        min = scan.nextInt();
+
+        while(max<=min){
+            System.out.print("inserisci il valore max della numerazione (il valore deve essere superiore a min): ");
+            max = scan.nextInt();
+        }
+
+        System.out.println(DataHelper.getRandomInteger(size, min, max));
+    }
+
+    public static void getRandomUniqueInteger() {
+        int size = 12;
+        
+        while(size<=0 || size>11){
+            System.out.print("inserisci la grandezza della lista (un numero da 1 a 11): ");
+            size = scan.nextInt();
+        }
+
+        System.out.println("Lista di numeri random univoci da 0 a 10");
+        System.out.println(DataHelper.getRandomUniqueInteger(size));
+    }
+
+    public static void getRandomUniqueIntegerUntilMax() {
+        int max = 0, size = 0;
+        
+        while(size<=0){
+            System.out.print("inserisci la grandezza della lista (tutti i numeri eccetto 0): ");
+            size = scan.nextInt();
+        }
+
+        System.out.print("inserisci il valore max della numerazione: ");
+        max = scan.nextInt();
+
+        System.out.println(DataHelper.getRandomUniqueInteger(size, max));
+    }
 
 
+    public static void getRandomUniqueIntegerBetweenMinMax() {
+        int min = 0, max = 0, size = 0;
+        
+        while(size<=0){
+            System.out.print("inserisci la grandezza della lista (tutti i numeri eccetto 0): ");
+            size = scan.nextInt();
+        }
 
+        System.out.print("inserisci il valore min della numerazione: ");
+        min = scan.nextInt();
+
+        while(max<=min){
+            System.out.print("inserisci il valore max della numerazione (il valore deve essere superiore a min): ");
+            max = scan.nextInt();
+        }
+        
+        System.out.println(DataHelper.getRandomUniqueInteger(size, min, max));
+    }
+
+    public static void testFrequencyMap() {
+        System.out.print("Inserisci la grandezza della lista di numeri casuali per la mappa: ");
+        int size = scan.nextInt();
+
+        List<Integer> randomNumbers = DataHelper.getRandomInteger(size);
+        System.out.println("Lista di numeri casuali generati: " + randomNumbers);
+
+        Map<Integer, Integer> mapF = DataHelper.getFrequencyMap(randomNumbers);
+        System.out.println(DataHelper.frequencyMapToString(mapF));
     }
 }
